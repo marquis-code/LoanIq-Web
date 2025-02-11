7e3709641df6212f8dd00114343267c1aa1acc4b
Marquis <abahmarquis@gmail.com>
2025-02-11 10:15:08 -0800

    fexed

--- composables/modules/wallet/useDebitWallet.ts
+++ composables/modules/wallet/useDebitWallet.ts
@@ -1,61 +1,3 @@
-// import { ref } from "vue";
-// import { wallet_api } from "@/api_factory/modules/wallet";
-// import { useCustomToast } from "@/composables/core/useCustomToast";
-
-// const payload = ref({
-//   amount: '',
-//   destinationBankCode: "",
-//   destinationBankName: "",
-//   destinationAccountNumber: "",
-//   destinationAccountName: "",
-//   narration: "",
-// });
-// const loading = ref(false);
-
-// export const useDebitWallet = () => {
-//   // const loading = ref(false);
-//   const { showToast } = useCustomToast();
-
-//   const debitWallet = async () => {
-//     loading.value = true;
-    
-//       const res = await  wallet_api.$_debit_wallet(payload.value)
-//        console.log(res, 'tres here')
-//       if(res.status === 201 || res.status === 201){
-//         showToast({
-//           title: "Success",
-//           message: res?.data?.message,
-//           toastType: "success",
-//           duration: 3000,
-//         });
-//         return res;
-//       } else {
-//         showToast({
-//           title: "Error",
-//           message: res.data.message || "Transfer Failed",
-//           toastType: "error",
-//           duration: 3000,
-//         });
-//         return res;
-//       }
-
-//       loading.value = false
-
-//   };
-
-//   const setPayload = (data: any) => {
-//     payload.value = { ...data };
-//   };
-
-//   return {
-//     debitWallet,
-//     loading,
-//     payload,
-//     setPayload,
-//   };
-// };
-
-
 import { ref } from "vue";
 import { wallet_api } from "@/api_factory/modules/wallet";
 import { useCustomToast } from "@/composables/core/useCustomToast";
@@ -80,13 +22,13 @@ export const useDebitWallet = () => {
       const res = await wallet_api.$_debit_wallet(payload.value);
       console.log(res, "tres here");
 
-      if (res.status === 201) {
-        showToast({
-          title: "Success",
-          message: res?.data?.message,
-          toastType: "success",
-          duration: 3000,
-        });
+      if (res.status === 201 || res.status === 201) {
+        // showToast({
+        //   title: "Success",
+        //   message: res?.data?.message,
+        //   toastType: "success",
+        //   duration: 3000,
+        // });
       } else {
         showToast({
           title: "Error",
